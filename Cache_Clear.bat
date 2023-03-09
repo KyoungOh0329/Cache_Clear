8862009643ef472c732a12aff9f11dbe996ace24
Jung KyoungOh <92211411+KyoungOh0329@users.noreply.github.com>
2023-03-09 12:47:12 +0900

    Update_Cache_Clear

---
+++ Cache_Clear.bat
@@ -0,0 +1,24 @@
+@echo off
+echo Clearing browser cache, history, and internet usage...
+REM Chrome
+rd /s /q "%LOCALAPPDATA%\Google\Chrome\User Data\Default\Cache"
+del /q /s "%LOCALAPPDATA%\Google\Chrome\User Data\Default\History"
+del /q /s "%LOCALAPPDATA%\Google\Chrome\User Data\Default\Cookies"
+
+echo Clearing Edge browser cache...
+echo.
+
+rem clear Edge browser cache
+cd "C:\Users\user\AppData\Local\Microsoft\Edge\User Data\PnaclTranslationCache"
+del /q /s *
+
+echo Edge browser cache cleared successfully.
+echo.
+
+rem clear Internet Explorer cache
+echo Clearing Internet Explorer cache...
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 255
+
+echo Internet Explorer cache cleared successfully.
+echo.
+pause
